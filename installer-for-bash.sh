eae777ec1e844f1ae25a6672134f3cc5facbb329
gventurini <giacomo.venturini3@gmail.com>
2022-04-03 22:07:59 +0200

    add installer for bash

---
+++ installer-for-bash.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# gets the absolute path to the directory containing this script
+DIR="$(dirname "$(readlink -f "$0")")"
+
+SCRIPT="$DIR/get-git-branch-name.sh"
+
+if [ -f "$SCRIPT" ]; then
+    # adds to .bashrc
+    # # get git branch name
+    # if [ -f "$SCRIPT" ]; then
+    #     source "$SCRIPT"
+    # fi
+    printf '\n%s\n%s\n%s\n%s\n' \
+        "# get git branch name" \
+        "if [ -f \"$SCRIPT\" ]; then" \
+        "    source \"$SCRIPT\"" \
+        'fi' \
+        >> "$HOME/.bashrc"
+fi
