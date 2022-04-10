9e0683b67366c7423f02e2fc48fab1d9ed57c476
gventurini <giacomo.venturini3@gmail.com>
2022-04-10 15:46:08 +0200

    fix shellcheck warning

--- get-git-branch-name.sh
+++ get-git-branch-name.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 function get_git_branch_name {
-  local branch_name=$(git rev-parse --abbrev-ref HEAD 2>/dev/null)
+  local branch_name
+  branch_name=$(git rev-parse --abbrev-ref HEAD 2>/dev/null)
   # -n checks if the command result length is not empty
   if [ -n "$branch_name" ]; then
     echo " [$branch_name]"
