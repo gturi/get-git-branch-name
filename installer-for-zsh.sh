b971c26937d4788ec0ccf1c60a64b78d047c2083
gventurini <giacomo.venturini3@gmail.com>
2025-04-06 15:41:00 +0200

    remove tabs and zsh bash emulation

--- installer-for-zsh.sh
+++ installer-for-zsh.sh
@@ -6,15 +6,13 @@ DIR="$(dirname "$(readlink -f "$0")")"
 SCRIPT="$DIR/get-git-branch-name.sh"
 
 if [ -f "$SCRIPT" ]; then
-    # adds to .zshrc
-    # # get git branch name
-    # autoload bashcompinit && bashcompinit
-    # emulate sh -c '. "$SCRIPT"'
-    printf '\n%s\n%s\n%s\n%s\n%s\n' \
-        "# get git branch name" \
-        "if [ -f \"$SCRIPT\" ]; then" \
-        '  autoload bashcompinit && bashcompinit' \
-        "  emulate sh -c '. \"$SCRIPT\"'" \
-        'fi' \
-        >> "$HOME/.zshrc"
+  # adds to .zshrc
+  # # get git branch name
+  # . "$SCRIPT"
+  printf '\n%s\n%s\n%s\n%s\n' \
+    "# get git branch name" \
+    "if [ -f \"$SCRIPT\" ]; then" \
+    "  . \"$SCRIPT\"" \
+    'fi' \
+    >>"$HOME/.zshrc"
 fi
