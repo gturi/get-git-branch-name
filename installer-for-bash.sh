ff9b478b9e59b000415b6ccc56cd81cf9107af54
gventurini <giacomo.venturini3@gmail.com>
2025-04-09 23:50:20 +0200

    add prompt to ask user to proceed with setup of .bashrc and .zshrc

--- installer-for-bash.sh
+++ installer-for-bash.sh
@@ -5,6 +5,16 @@ DIR="$(dirname "$(readlink -f "$0")")"
 
 SCRIPT="$DIR/get-git-branch-name.sh"
 
+read -p "get-git-branch-name: do you wish to add custom-prompt to your .bashrc? [y/n]" -n 1 -r
+echo # moves to a new line
+
+if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+  log "get-git-branch-name: .bashrc setup aborted"
+  # handle exits from shell or function but don't exit interactive shell
+  [[ "$0" = "$BASH_SOURCE" ]] && exit 0 || return 0
+fi
+
+
 if [ -f "$SCRIPT" ]; then
   # adds to .bashrc
   # # get git branch name
